62b194f083bbf69e72ea3397564b338a6b2f5aa3
Raphaël <raphael.mabille@epitech.eu>
2026-04-08 06:29:11 +0000

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
+import path from "path"
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/raphael-mabille.github.io/',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: '/Raphael-Mabille-portfolio/',
 })
